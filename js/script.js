e8adc5fc3190bcd82768debdddb06c34df27fbdf
Mike <mishahorod@gmail.com>
2022-08-04 16:47:48 +0300

    last commit befour present

--- js/script.js
+++ js/script.js
@@ -1,14 +1,3 @@
-// $('.contact-call').on('click', function() {
-//     $('.popup__wrapper').addClass('active');
-// });
-
-// $('.popup__wrapper').on('click', function(){
-//     $('.popup__wrapper').removeClass('active');
-// });
-
-
-
-
 if ($(window).width() < 650) {   
     $('.bio-second__content').addClass('owl-carousel');
 } else {
@@ -22,3 +11,73 @@ $(document).ready(function(){
         nav: true
     });
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// document.addEventListener('DOMContentLoaded', function() {
+//     const form = document.getElementById('form');
+//     form.addEventListener('submit', formSend);
+
+//     async function formSend(e) {
+//         e.preventDefault();
+
+//         let error = formValidate(form);
+
+//     }
+
+
+//     function formValidate(form) {
+//         let error = 0;
+//         let formReq = document.querySelectorAll('._req');
+
+
+//         for(let index = 0; index < formReq.length; index++) {
+//             const input = form.Req[index];
+//             formRemoveError(input);
+
+
+//             if(input.classList.contains('_email')) {
+//                 if(emailTest(input)) {
+//                     formAddError(input);
+//                     error++;
+//                 }
+//             }else {
+//                 if(input.value === ''){
+//                     formAddError(input);
+//                     error++;
+//                 }
+//             }
+
+//         }
+//     }
+
+//     function formAddError(input) {
+//         input.parentElement.classList.add('_error');
+//         input.classList.add('_error');
+//     }
+//     function formRemoveError(input) {
+//         input.parentElement.classList.remove('_error');
+//         input.classList.remove('_error');
+//     }
+
+
+
+//     function emailTest(input) {
+//         return !/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,8})+$/.test(input.value);
+//     }
+
+
+
+// });
